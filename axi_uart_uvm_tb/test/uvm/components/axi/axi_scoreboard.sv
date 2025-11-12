c6b229a4e511701d98d833273992f0d533fc290c
Tahmeed Reza <tahmeedreza@gmail.com>
2025-11-12 16:19:55 +0000

    axi monitor and scoreboard update

--- axi_uart_uvm_tb/test/uvm/components/axi/axi_scoreboard.sv
+++ axi_uart_uvm_tb/test/uvm/components/axi/axi_scoreboard.sv
@@ -18,6 +18,13 @@ class axi_scoreboard extends uvm_scoreboard;
     uvm_analysis_imp_tx  #(uart_tx_item, axi_scoreboard) uart_tx_analysis_imp;
     uvm_analysis_imp_rx  #(uart_rx_item, axi_scoreboard) uart_rx_analysis_imp;
 
+    uvm_tb_axi_req_t    axi_config_req [$];
+    uvm_tb_axi_resp_t   axi_config_resp [$];
+    uvm_tb_axi_req_t    axi_tx_data_req [$];
+    uvm_tb_axi_resp_t   axi_tx_data_resp [$];
+    uvm_tb_axi_req_t    axi_rx_data_req [$];
+    uvm_tb_axi_resp_t   axi_rx_data_resp [$];
+
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         m_analysis_imp = new("m_analysis_imp", this);
@@ -26,6 +33,14 @@ class axi_scoreboard extends uvm_scoreboard;
     endfunction
 
     virtual function void write_axi(dut_seq_item item);
+        `uvm_info(get_name(), $sformatf("axi_write_item: %p\ndata: %p\nresp: %p\n", item.axi_req.aw, item.axi_req.w, item.axi_resp.b), UVM_LOW);
+        `uvm_info(get_name(), $sformatf("axi_read_item: %p\nresp: %p\n", item.axi_req.ar, item.axi_resp.r), UVM_LOW);
+        if (item.axi_req.aw.addr == 16'h14) begin
+            axi_tx_data_req.push_back(item.axi_req);
+            axi_tx_data_resp.push_back(item.axi_resp);
+        end else begin
+            
+        end
     endfunction
 
     virtual function void write_tx(uart_tx_item item);
